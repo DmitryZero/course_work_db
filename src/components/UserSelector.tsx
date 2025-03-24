171fed16524df15ffcee31f93e2fb7c5550be691
John Doe <johndoe@email.com>
2025-03-24 21:58:48 +0400

    Скоректировал Выбор пользователя

--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { TUser } from "@/interfaces/TUser";
+import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material";
 import { useState } from "react";
 
 type TProps = {
@@ -9,18 +10,27 @@ type TProps = {
 export default function UserSelector({ users }: TProps) {
     const [currentUser, setCurrentUser] = useState(users[0]);
 
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const selected_user = users.find(u => u.id === (event.target as HTMLInputElement).value);
+        console.log(selected_user)
+        setCurrentUser(selected_user!);
+    };
+
     return (
-        <div>
-            <label className="block font-medium">Выберите пользователя:</label>
-            <select
-                className="p-2 border rounded"
-                value={currentUser.id}
-                onChange={(e) => setCurrentUser(users.find(u => u.id == e.target.value)!)}
-            >
-                {users.map((user) => (
-                    <option key={user.id} value={user.id}>{user.name}</option>
-                ))}
-            </select>
+        <div className="border-1 border-b-amber-900 p-2 rounded-2xl">
+            <FormControl className="">
+                <FormLabel id="demo-radio-buttons-group-label">Текущий пользователь</FormLabel>
+                <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    defaultValue="1"
+                    name="radio-buttons-group"
+                    onChange={handleChange}
+                >
+                    {users.map((user) => (
+                        <FormControlLabel value={user.id} control={<Radio />} label={user.name} />
+                    ))}
+                </RadioGroup>
+            </FormControl>
         </div>
     );
 }
